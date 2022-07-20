706ed07d6883bcce110dc9f483bf4045071a5383
eliseSchultz <elise@pop-os.localdomain>
2022-07-20 11:38:30 -0500

    removed i index because it wasnt being used

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -7,8 +7,6 @@ class TicTacToe
 
     # row checks
 
-    # column index
-    i = 0
     @board.each do |row|
       # row index
       j = 0
@@ -26,34 +24,10 @@ class TicTacToe
         end
         j += 1
       end
-      i += 1
     end
 
     # column checks
 
-    # if row1[0] == "o" && row2[0] == "o" && row3[0] == "o"
-    #   return "o"
-    # end
-
-    # if row1[1] == "o" && row2[1] == "o" && row3[1] == "o"
-    #   return "o"
-    # end
-
-    # if row1[2] == "o" && row2[2] == "o" && row3[2] == "o"
-    #   return "o"
-    # end
-
-    # if row1[0] == "x" && row2[0] == "x" && row3[0] == "x"
-    #   return "x"
-    # end
-
-    # if row1[1] == "x" && row2[1] == "x" && row3[1] == "x"
-    #   return "x"
-    # end
-
-    # if row1[2] == "x" && row2[2] == "x" && row3[2] == "x"
-    #   return "x"
-    # end
 
     # diagonal checks
 
